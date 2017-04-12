5ae3e0a4a1ec240d71feb748b06bdb4b7e8c9866
Dmitriy <funnybalmosh@gmail.com>
2017-04-12 19:02:50 +0300

    XML Desirialization

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadData();
+        }
+
+        private static void ReadData()
+        {
+            WebClient web = new WebClient();
+            string s = web.DownloadString(new Uri("http://...", UriKind.Absolute));
+            XmlSerializer xml = new XmlSerializer(typeof(RssClass));
+            RssClass res = xml.Deserialize(new MemoryStream(Encoding.UTF8.GetBytes(s))) as RssClass; 
+        }
+    }
+}
---
+++ ConsoleApp1/RssClass.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace ConsoleApp1
+{
+
+    [XmlRoot(ElementName = "rss")]
+    public class RssClass
+    {
+        [XmlElement(ElementName = "channel")]
+        public ChanelClass Channel { get; set; }
+    }
+
+    public class ChanelClass
+    {
+        [XmlElement(ElementName = "Item")]
+        public List<ItemClass> Items { get; set; }
+    }
+
+    public class ItemClass
+    {
+        [XmlElement(ElementName = "title")]
+        public string Title { get; set; }
+    }
+
+}
